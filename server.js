4efc09053869e423e7133090fa6e23c64437691b
aaaaayushh <aayushshah1142@gmail.com>
2021-01-16 17:11:41 +0530

    please work

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const exerciseRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exerciseRouter);
+app.use("/users", usersRouter);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+app.listen(port, () => {
+  console.log(`Server is running on port :${port}`);
+});
